0565aced23c137707ff092a5754492d03348baff
Roa <Conreas@mailinator.com>
2024-05-31 11:16:34 -0500

    MigrationsDeleteTableCabeceraPLantilla

---
+++ Conciliador.Datos/Migrations/20240531161327_DeleteTableCabeceraPlantilla.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Conciliador.Datos.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeleteTableCabeceraPlantilla : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CabeceraPlantilla");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CabeceraPlantilla",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreadoPor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EditadoPor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EliminadoPor = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Estado = table.Column<bool>(type: "bit", nullable: false),
+                    EstadoRegistro = table.Column<int>(type: "int", nullable: false),
+                    FechaCreacion = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FechaEdicion = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FechaEliminacion = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CabeceraPlantilla", x => x.Id);
+                });
+        }
+    }
+}
